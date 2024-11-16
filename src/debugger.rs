f275ffb60368bf5380d3066277e4bc52c0352dbc
Jakob <jakobklocker@hotmail.com>
2024-11-16 21:12:52 +0100

    reading commands, waitpid set up

--- src/debugger.rs
+++ src/debugger.rs
@@ -1,5 +1,6 @@
 use crate::process;
 use nix::sys::wait::{waitpid, WaitStatus};
+use std::io;
 pub struct Debugger {
     process: process::Process,
     // breakpoint later here
@@ -12,10 +13,54 @@ impl Debugger {
         }
     }
 
+    fn get_command(&self) -> String{
+        println!("Enter Command: ");
+
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+        let command = command.trim();
+        println!("{}", command);
+        return command.to_string();
+    }
+
+    fn handle_command(&self, command: &str){
+        let mut parts = command.split_whitespace();
+        let command_word = parts.next();
+        match command_word {
+            Some("breakpoint") => {
+                if let Some(arg) = parts.next(){
+                    let breakpoint_addr: i32 = arg.parse().unwrap();
+                    self.breakpoint(breakpoint_addr);  
+                    
+                } else {
+                    println!("No seconda argument provided for breakpoint");
+                }
+            },
+            Some("continue") => self.cont(),
+            Some("exit") => self.exit(),
+            _ => println!("command not found {}", command),
+        }
+    }
+
+    fn breakpoint(&self, id: i32) {
+        println!("Setting breakpoint at {}", id);
+    }
+    
+    fn cont(&self) {
+        println!("Continuing execution...");
+    }
+    
+    fn exit(&self) {
+        println!("Exiting the debugger...");
+    }
+    
+
     pub fn run(&self){
         loop {
             let status = waitpid(self.process.pid, None);
-    
+            
+            let command = self.get_command();
+            self.handle_command(command.as_str());
             
 
 
@@ -43,4 +88,6 @@ impl Debugger {
         }
         println!("Debugger run complete.");
     }
+
+
 }
