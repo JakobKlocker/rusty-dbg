609b60207f3194aa6f3d6e02568c5ed55dd1c83b
Jakob <jakobklocker@hotmail.com>
2024-11-18 21:32:35 +0100

    reading out breakpoint hex working, added some error checks

--- src/debugger.rs
+++ src/debugger.rs
@@ -32,9 +32,16 @@ impl Debugger {
         match command_word {
             Some("breakpoint") => {
                 if let Some(arg) = parts.next(){
-                    let breakpoint_addr: u64 = arg.parse().unwrap();
-                    self.breakpoint.set_breakpoint(breakpoint_addr, self.process.pid);
-                    
+
+                    match u64::from_str_radix(arg, 16){
+                        Ok(breakpoint_addr)=> {
+                            println!("{}", breakpoint_addr);
+                            self.breakpoint.set_breakpoint(breakpoint_addr, self.process.pid);
+                        }
+                        Err(e) => {
+                            println!("breakpoint failed: {}", e);
+                        }
+                    }
                 } else {
                     println!("No seconda argument provided for breakpoint");
                 }
@@ -47,21 +54,18 @@ impl Debugger {
     
     fn cont(&self) {
         println!("Continuing execution...");
-        ptrace::cont(self.process.pid, None).expect("hi");
+        ptrace::cont(self.process.pid, None).expect("Cont fucntion failed");
     }
     
     fn exit(&self) {
         println!("Exiting the debugger...");
+        std::process::exit(0);
     }
     
 
     pub fn run(&mut self){
         loop {
             let status = waitpid(self.process.pid, None);
-            
-            let command = self.get_command();
-            self.handle_command(command.as_str());
-
             match status {
                 Ok(WaitStatus::Exited(_, exit_status)) => {
                     println!("Process exited with status: {}", exit_status);
@@ -69,7 +73,8 @@ impl Debugger {
                 }
                 Ok(WaitStatus::Stopped(_, signal)) => {
                     println!("Process stopped by signal: {:?}", signal);
-                
+                    let command = self.get_command();
+                    self.handle_command(command.as_str());
                 }
                 Ok(WaitStatus::Signaled(_, signal, _)) => {
                     println!("Process terminated by signal: {:?}", signal);
